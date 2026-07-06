99ce41591239334350d44eea240546c696f22cef
saviozoboli <savio.zoboli@gmail.com>
2026-07-06 13:39:39 -0300

    feat: adicionado logos de diferentes tipos e criado componente de troca de tema

---
+++ src/hmi/src/app/components/theme-toggler/theme-toggler.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-toggler',
+  imports: [],
+  templateUrl: './theme-toggler.html',
+  styleUrl: './theme-toggler.scss',
+})
+export class ThemeToggler {
+
+}
